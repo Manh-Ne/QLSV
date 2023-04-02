77acbd473e8270d5e4d9458ff1a06502e30827f9
Manh-Ne <manhnguyenquang13@gmail.com>
2023-04-02 14:51:55 +0700

    push

---
+++ app/src/main/java/com/example/studentmanagement/ActivitySubject.java
@@ -0,0 +1,107 @@
+package com.example.studentmanagement;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.studentmanagement.adapter.adaptersubject;
+import com.example.studentmanagement.database.database;
+import com.example.studentmanagement.model.Subject;
+
+import java.util.ArrayList;
+
+public class ActivitySubject extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ListView listViewsubject;
+    ArrayList<Subject> ArrayListSubject;
+    com.example.studentmanagement.database.database database;
+    com.example.studentmanagement.adapter.adaptersubject adaptersubject;
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+
+        toolbar = findViewById(R.id.toolbarSubject);
+        listViewsubject = findViewById(R.id.listviewSubject);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        database = new database(this);
+
+        ArrayListSubject = new ArrayList<>();
+
+        Cursor cursor = database.getDataSubject();
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(0);
+            String title = cursor.getString(1);
+            int credit = cursor.getInt(2);
+            String time = cursor.getString(3);
+            String place = cursor.getString(4);
+
+            ArrayListSubject.add(new Subject(id, title, credit, time, place));
+        }
+        adaptersubject = new adaptersubject(ActivitySubject.this,ArrayListSubject);
+        listViewsubject.setAdapter(adaptersubject);
+        cursor.moveToFirst();
+        cursor.close();
+        //tạo sự kueenj click vào item subjcet
+        listViewsubject.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ActivitySubject.this,ActivityStudent.class);
+                int id_subject = ArrayListSubject.get(i).getId();
+                //truyền dữ lieeuk id subjcet vào student
+                intent.putExtra("id subject",id_subject);
+                startActivity(intent);
+            }
+        });
+
+    }
+    //Thêm một menu add vào toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuadd,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            //nếu click vào add thì chuyển qua màn hinhg add subject
+            case R.id.menuadd:
+                Intent intent1 = new Intent(ActivitySubject.this,ActivityAddSubject.class);
+                break;
+
+                //Nếu click vào nút còn lại là nút back thì quay lại main
+            default:
+                Intent intent = new Intent(ActivitySubject.this,MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    // nếu click back ở ddienj tohaij sẽ trở về main activyty
+    @Override
+    public void onBackPressed() {
+        count++;
+        if (count>=1){
+            Intent intent = new Intent(ActivitySubject.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/studentmanagement/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.studentmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSubject,btnAuthor,btnExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAuthor = findViewById(R.id.buttonAuthor);
+        btnExit = findViewById(R.id.buttonExit);
+        btnSubject = findViewById(R.id.buttonSubject);
+        //Click tác giả
+        btnAuthor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogAuthor();
+            }
+        });
+        // click subject
+        btnSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //chuyển qua activity subject
+                Intent intent = new Intent(MainActivity.this,ActivitySubject.class);
+                startActivity(intent);
+            }
+        });
+        //click exit app
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogExit();
+            }
+        });
+    }
+// hàm hiển thị cửa sổ dialog exit
+    private void DialogExit() {
+        Dialog dialog = new Dialog(this);
+
+        dialog.setContentView(R.layout.dialogexit);
+        // tắt click ngoài là thoát
+        dialog.setCanceledOnTouchOutside(false);
+
+        Button btnYes = dialog.findViewById(R.id.buttonYes);
+        Button btnNo = dialog.findViewById(R.id.buttonNo);
+
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                // thoát
+                Intent intent1 = new Intent(Intent.ACTION_MAIN);
+                intent1.addCategory(Intent.CATEGORY_HOME);
+                startActivity(intent1);
+            }
+        });
+        //nếu click no thì đóng cửa sổ
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
+    }
+
+    // Hiển thị thông tin tác giả
+    private void DialogAuthor() {
+        //tạo đồi tg cửa sổ dialog
+        Dialog dialog = new Dialog(this);
+    //nạp layout vào dialog
+        dialog.setContentView(R.layout.dialoginformation);
+        dialog.show();
+    }
+}
